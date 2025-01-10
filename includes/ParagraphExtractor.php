4e617bc5046dbf8df3dc7c9bb7fd82df08218ad4
Edward Chernenko <edwardspec@gmail.com>
2025-01-10 21:11:16 +0300

    Preliminary port of JS findText() to ParagraphExtractor::findSnippet()

--- includes/ParagraphExtractor.php
+++ includes/ParagraphExtractor.php
@@ -59,16 +59,6 @@ class ParagraphExtractor {
 	}
 
 	/**
-	 * Search this page for all paragraphs that contain $textToFind or its parts.
-	 * @param string $textToFind Arbitrary string, e.g. "Sentence is a sequence of words."
-	 * @return string List of paragraph numbers, e.g. "1-7,10-12,15".
-	 */
-	public function findSnippet( $textToFind ) {
-		/* TODO */
-		return '';
-	}
-
-	/**
 	 * Parse this page and split it into paragraphs. Returns array of innerText of every paragraph.
 	 * @return string[]
 	 */
@@ -167,5 +157,151 @@ class ParagraphExtractor {
 		}
 
 		return implode( ',', $packed );
+	}
+
+	/**
+	 * Search this page for all paragraphs that contain $textToFind or its parts.
+	 * @param string $textToFind Arbitrary string, e.g. "Sentence is a sequence of words."
+	 * @return string List of paragraph numbers, e.g. "1-7,10-12,15".
+	 */
+	public function findSnippet( $textToFind ) {
+		if ( !$textToFind ) {
+			return '';
+		}
+
+		$paragraphs = $this->getAllParagraphs();
+		if ( !$paragraphs ) {
+			return '';
+		}
+
+		// Settings, TODO: add documentation.
+		$recursionLimit = 50;
+		$partInTooManyParagraphsLimit = 5;
+		$entireSnippetInTooManyParagraphsLimit = 12;
+
+		// Remove quotes from the snippet, so that behavior without CirrusSearch would be the same.
+		$textToFind = $this->normalizeTextForSearch( $textToFind );
+		$paragraphs = array_map( function ( $text ) {
+			return $this->normalizeTextForSearch( $text );
+		}, $paragraphs );
+
+		$words = explode( ' ', $textToFind );
+		$results = [];
+		$limit = 0;
+
+		while ( $words ) {
+			$result = $this->findWordsRecursive( $words, $paragraphs );
+			if ( !$result ) {
+				break;
+			}
+
+			$words = $result['leftoverWords'];
+			$result['parNumbers'] = array_keys( $result['paragraphs'] );
+
+			if ( count( $result['parNumbers'] ) <= $partInTooManyParagraphsLimit ) {
+				// New usable result.
+				$results[] = $result;
+			} else {
+				// This match is useless (too many paragraphs), so we should discard it.
+				// However, if one of these matched paragraphs directly follows the paragraph
+				// from the previous match, this 1 paragraph is still useful.
+				if ( $results ) {
+					$prev = $results[count( $results ) - 1];
+					$prevParCount = count( $prev['parNumbers'] );
+					if ( $prevParCount ) {
+						$extraParNumber = $prev['parNumbers'][$prevParCount - 1] + 1;
+						if ( in_array( $extraParNumber, $result['parNumbers'] ) ) {
+							// This result is still useful.
+							$prev['parNumbers'][] = $extraParNumber;
+						}
+					}
+				}
+			}
+
+			if ( $limit++ > $recursionLimit ) {
+				// console.log( 'findpar.js: Depth limit reached.' );
+				break;
+			}
+		}
+
+		if ( count( $results ) > $entireSnippetInTooManyParagraphsLimit ) {
+			// console.log( 'findpar.js: found too many paragraphs (' + results.length +
+			//	'), discarding all matches (they are likely incorrect).' );
+			return '';
+		}
+
+		// Get all paragraph numbers (sorted and unique).
+		$parNumbers = [];
+		foreach ( $results as $result ) {
+			$parNumbers = array_merge( $parNumbers, $result['parNumbers'] );
+			// console.log( 'findpar.js: found paragraphs: query=' + result.query +
+			//	', parNumbers=[' + result.parNumbers.join( ',' ) +
+			//	'], leftoverWords=' + result.leftoverWords );
+		}
+		$parNumbers = array_unique( $parNumbers );
+		sort( $parNumbers );
+
+		return $this->packParNumbers( $parNumbers );
+	}
+
+	/**
+	 * Searches $paragraphs for the longest sequence of strings in $words array.
+	 *
+	 * @param string[] $words Full prompt, e.g. [ "Sentence", "consists", "of", "words." ].
+	 * @param string[] $paragraphs Paragraphs to be searched.
+	 * @param string $oldQuery Previously found string, e.g. "Sequential words can form a".
+	 * @return mixed Search result. If not null ("not found"), contains the following keys:
+	 * string query Longest sequence of words from "words" array that have been found.
+	 * string[] paragraphs Paragraphs where "query" was found.
+	 * string[] leftoverWords Remaining words from "words" array that haven't been found yet.
+	 */
+	public function findWordsRecursive( array $words, array $paragraphs, $oldQuery = '' ) {
+		if ( !$words ) {
+			// Empty prompt.
+			return null;
+		}
+
+		$query = $oldQuery . ( $oldQuery ? ' ' : '' ) . array_shift( $words );
+
+		$found = array_filter( $paragraphs, static function ( $text ) use ( $query ) {
+			return str_contains( $text, $query );
+		} );
+		if ( !$found ) {
+			// Not found in any of the paragraphs.
+			if ( $oldQuery === '' ) {
+				// Discard the word that wasn't found, try again from the next word.
+				return $this->findWordsRecursive( $words, $paragraphs, $oldQuery );
+			}
+			return null;
+		}
+
+		$foundBetter = $this->findWordsRecursive( $words, $found, $query );
+		if ( $foundBetter ) {
+			// Succeeded at adding more words.
+			return $foundBetter;
+		}
+
+		return [
+			// Sentence that was found.
+			'query' => $query,
+
+			// Where it was found.
+			'paragraphs' => $found,
+
+			// These words are not a continuation of already found sentence "query".
+			'leftoverWords' => $words
+		];
+	}
+
+	/**
+	 * Remove unnecessary parts of snippet/text that can interfere with matching,
+	 * such as quotes, newlines, excessive whitespace, etc.
+	 *
+	 * @param string $text
+	 * @return string
+	 */
+	public function normalizeTextForSearch( $text ) {
+		$text = str_replace( [ '"', "'" ], '', $text );
+		return preg_replace( '/\s+/', ' ', $text );
 	}
 }
