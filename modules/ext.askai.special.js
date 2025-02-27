e041d436b529e87ff79e86fa36e614b89be11d74
Edward Chernenko <edwardspec@gmail.com>
2025-02-27 06:33:26 +0300

    When AI includes source number in the response, show a link to the page

--- modules/ext.askai.special.js
+++ modules/ext.askai.special.js
@@ -60,6 +60,27 @@ $( function () {
 	 * @param {boolean} isSuccess True if this is a successful response, false for error.
 	 */
 	function showResponse( $todo, responseText, isSuccess ) {
+		const pageNames = $pages.val().split( '\n' );
+
+		// If response from the AI mentions things like "Source #1",
+		// convert them into clickable links to the articles/paragraphs
+		// that were listed in the field "List of wiki pages".
+
+		responseText = responseText.replace( /\((?:Source #)?([0-9]+)\)/g, function ( matchedText, sourceNumber ) {
+			const linkTarget = pageNames[sourceNumber - 1];
+			if ( !linkTarget ) {
+				// Some unrelated number (either 0 or greater than the number of page names).
+				return matchedText;
+			}
+
+			// Show the link
+			const title = new mw.Title( linkTarget );
+			const $link = $( '<a>' )
+				.attr( 'href', title.getUrl() )
+				.append( matchedText );
+			return $link[0].outerHTML;
+		} );
+
 		const $answer = $( '<p>' ).attr( 'class', 'mw-askai-answer' ).append( responseText );
 		$todo.removeClass( 'mw-askai-prompt-loading' )
 			.addClass( isSuccess ? 'mw-askai-prompt-loaded' : 'mw-askai-prompt-failed' )
