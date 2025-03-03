3b04b891ce28c52765145bb9e999b9e07340c2d2
Edward Chernenko <edwardspec@gmail.com>
2025-03-03 22:37:17 +0300

    If the URL has #par123, scroll to it after adding the paragraph anchors

--- modules/ext.askai.addparlinks.js
+++ modules/ext.askai.addparlinks.js
@@ -8,4 +8,12 @@ $( function () {
 			.attr( 'id', 'par' + idx )
 		);
 	} );
+
+	const anchor = window.location.hash;
+	if ( anchor.match( /^#par[0-9]+$/ ) ) {
+		// When user loads the page with #par123 in its URL,
+		// scrolling doesn't happen naturally, because this anchor doesn't exist yet.
+		// Now that we have just added this anchor, we can perform the scrolling.
+		$( anchor )[ 0 ].scrollIntoView();
+	}
 } );
